d32be9f7466a3ca43edf879d462cb781617660aa
kt08 <dinhthi1440@gmail.com>
2024-11-14 11:46:55 +0700

    add eureka server

---
+++ EurekaServerProj/src/test/java/com/example/eurekaserverproj/EurekaServerProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaserverproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
